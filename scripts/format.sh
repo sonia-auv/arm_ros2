cc457e9974118ee1cd390c2ab3077bb96686c980
ArthurPV <arthurpontiervalois@gmail.com>
2026-01-25 23:38:26 -0500

    Update scripts/format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,5 +3,5 @@
 set -e
 set -o pipefail
 
-clang-format -i $(find include -type f -name "*.hpp")
-clang-format -i $(find src -type f -name "*.cpp")
+clang-format -i $(find packages -type f -name "*.hpp")
+clang-format -i $(find packages -type f -name "*.cpp")
